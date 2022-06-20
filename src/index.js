0a254b358ae861672341cd5a2cbb1ffdc2dd5bd5
chrissiku <chrissiku5@gmail.com>
2022-06-20 16:17:08 +0200

    Webpack Configuration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+import _ from "lodash";
+import "./css/style.css";
+import Project from "./assets/images/project.jpg";
+
 const main = document.querySelector("#main");
 
-main.innerHTML = `Hello word!`;
+main.innerHTML = _.join(["webpack", "config"], " ");
+
+const myProject = new Image();
+myProject.src = Project;
+
+main.appendChild(myProject);
