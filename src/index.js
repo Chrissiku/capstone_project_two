8b3135e99e27df466db2c8cbd898502e9f02cdb1
bruk19 <brukteshome21@gmail.com>
2022-06-21 15:58:22 +0300

    Add basic html structure for navigation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@
 /* eslint-disable quotes */
 import _ from "lodash";
 import "./css/style.css";
-import Project from "./assets/images/project.jpg";
 
-const main = document.querySelector("#main");
 
-main.innerHTML = _.join(["webpack", "config"], " ");
-
-const myProject = new Image();
-myProject.src = Project;
-
-main.appendChild(myProject);
