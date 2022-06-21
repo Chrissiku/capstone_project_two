1dcb6b41f0422a1b58b6ddd24c8854d88b66a345
bruk19 <brukteshome21@gmail.com>
2022-06-22 01:06:45 +0300

    Fech movies list from external API and display it

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@
 import _ from "lodash";
 import "./css/style.css";
 
+fetch('https://api.tvmaze.com/seasons/1/episodes').then((data) => {
+    return data.json();
+}).then((completedata) => {
+    let data1="";
+    completedata.map((values) => {
+        data1+= `
+        <div class="main" id="main">
+        <img src=${values.image.medium} alt="img">
+        <h2 class="title">${values.name}</h2>
+        <button>comments</button>
+        <button>Reservation</button>
+      </div>`
+    }); 
+    document.getElementById('popup').innerHTML= data1;
+}).catch((err) => {
+    console.log(err);
+})
 
