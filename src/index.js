5a560bb558df5264c4fc8b5c59b0fbc002455bd4
bruk19 <brukteshome21@gmail.com>
2022-06-24 17:31:54 +0300

    Add fix linters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-
 /* eslint-disable quotes */
 import "./css/style.css";
 import renderMovies from "./modules/pageBody.js";
 import likeMovie from "./modules/likeFeature.js";
-
-renderMovies();
-const body = document.querySelector("body");
-
-body.addEventListener("click", () => {
-  likeMovie();
-});
 /* eslint-disable linebreak-style */
 /* eslint-disable max-len */
 /* eslint-disable linebreak-style */
@@ -19,10 +11,16 @@ body.addEventListener("click", () => {
 /* eslint-disable arrow-body-style */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable quotes */
-import "./css/style.css";
 
 import eventListners from "./utils/commentPopUp.js";
 import itemsCounter from "./utils/itemsCounter.js";
+
+renderMovies();
+const body = document.querySelector("body");
+
+body.addEventListener("click", () => {
+  likeMovie();
+});
 
 eventListners();
 itemsCounter();
